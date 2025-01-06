c40a4ed1f20bcc46b667c4711c6147c183bc9f5d
alana-jophy <alanapj16@gmail.com>
2025-01-06 10:09:29 +0000

    app.js

---
+++ app.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Molecule from '@dtinsight/molecule';
+import '@dtinsight/molecule/esm/style/mo.css';
+
+function App() {
+    return (
+        <div style={{ height: '100vh' }}>
+            <Molecule />
+        </div>
+    );
+}
+
+export default App;
